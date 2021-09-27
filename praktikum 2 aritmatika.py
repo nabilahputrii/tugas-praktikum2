b352490ce52876ab0c5f2fe8d8bc5673cfcf2649
nabilahputrii <91355858+nabilahputrii@users.noreply.github.com>
2021-09-27 15:03:17 +0700

    Add files via upload

---
+++ praktikum 2 aritmatika.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:10:20 2021
+
+@judul : praktikum 2, Operasi Aritmatika pada Python
+@hari tanggal : 27-09-2021
+@NIM : 065002100002
+@Nama : Nabilah Putri
+"""
+
+print("penghitugan aritmatika dalam python")
+a=float(input("masukan nilai umur kamu:"))
+b=float(input("masukan nilai umur dia:"))
+
+print("jumlah  ditambah umur dia adalah:",a+b)
+print("selisih antara umur kamu  dan bumur dia adalah:",a-b)
+print("jumlah dari umur kamu dikali umur dia adalah:",a*b)
+print("jumlah sisa pembagian dari hasil umur kamu dibagi umur dia adalah:",a%b)
+print("jumlah Pembagian umur kamu dengan umur dia:",a/b)
+print("hasil dari log (a)",a)
+print("hasil a pangkat b adalah:",a**b)
+
+print("terimakasih")
